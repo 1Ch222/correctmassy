0a707f97f6b288b033fcf7123561f47b6d0e0b92
1Ch222 <137825924+1Ch222@users.noreply.github.com>
2023-07-21 10:26:39 +0200

    Create corrective.py

---
+++ corrective.py
@@ -0,0 +1,35 @@
+import os
+from PIL import Image
+
+def convert_to_grayscale(input_path, output_path):
+    # Ouvrir l'image d'entrée
+    with Image.open(input_path) as img:
+        # Convertir l'image en niveaux de gris (L mode)
+        img_gray = img.convert("L")
+        
+        # Sauvegarder l'image en niveaux de gris, en écrasant l'image d'origine
+        img_gray.save(output_path)
+
+def process_images_in_folder(folder_path):
+    # Vérifier si le dossier existe
+    if not os.path.exists(folder_path):
+        print(f"Le dossier '{folder_path}' n'existe pas.")
+        return
+
+    # Parcourir tous les fichiers du dossier
+    for filename in os.listdir(folder_path):
+        if filename.endswith("labelIds.png"):
+            input_path = os.path.join(folder_path, filename)
+            output_path = os.path.join(folder_path, filename)
+
+            # Convertir l'image en niveaux de gris et la sauvegarder
+            convert_to_grayscale(input_path, output_path)
+
+            # Supprimer l'image d'origine
+            os.remove(input_path)
+
+            print(f"Image '{filename}' traitée et remplacée par l'image en niveaux de gris.")
+
+# Exemple d'utilisation
+input_folder_path = "chemin/vers/le/dossier"
+process_images_in_folder(input_folder_path)
